606c21f26aef66f6c45ec3356c848edbc6bded84
Jorian <jorian@outlook.com>
2017-11-22 22:32:56 +0100

    :o: RED new secret code should always be random

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GameTest {
@@ -8,5 +9,17 @@ public class GameTest {
     public void gameShouldHaveSecretCodeSet() {
         Game game = new Game();
         assertNotNull(game.getSecretCode());
+    }
+
+    @Test
+    public void eachNewSecretCodeShouldBeRandom() {
+        Game game = new Game();
+        game.setSecretCode();
+        Code firstCode = game.getSecretCode();
+
+        game.setSecretCode();
+        Code secondCode = game.setSecretCode();
+
+        assertNotEquals(firstCode, secondCode);
     }
 }
