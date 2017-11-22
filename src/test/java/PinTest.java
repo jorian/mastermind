707087fa85f5088f3dc1cd7e7884757c7ce3586d
Jorian <jorian@outlook.com>
2017-11-22 21:58:30 +0100

    :o: RED pin value more than 8 should throw exception

--- src/test/java/PinTest.java
+++ src/test/java/PinTest.java
@@ -17,6 +17,18 @@ public class PinTest {
         Pin pin = new Pin();
         pin.setValue(-4);
 
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void pinValueZeroShouldThrowException() {
+        Pin pin = new Pin();
+        pin.setValue(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void pinValueHigherThanEightShouldThrowException() {
+
+        Pin pin = new Pin();
+        pin.setValue(9);
     }
 }
