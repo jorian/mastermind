c89e92e6a062d43f23807928e85206078cc07bfe
Jorian <jorian@outlook.com>
2017-11-22 21:48:51 +0100

    :ballot_box_with_check: GREEN pin value is correct

---
+++ src/main/java/Pin.java
@@ -0,0 +1,15 @@
+public class Pin {
+    private int value;
+
+    Pin() {
+
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
