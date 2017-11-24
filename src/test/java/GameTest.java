0a50191d8db503bf3dc1461be33ef5c2a2245bbf
Jorian <jorian@outlook.com>
2017-11-24 14:54:02 +0100

    :o: RED each new code should be random

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -20,6 +21,6 @@ public class GameTest {
         game.setSecretCode();
         Code secondCode = game.getSecretCode();
 
-        assertNotEquals(firstCode, secondCode);
+        assertFalse(firstCode.equals(secondCode));
     }
 }
