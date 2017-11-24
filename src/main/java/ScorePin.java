dd83702903c66aec5544914c75b958dbdc4c2375
Jorian <jorian@outlook.com>
2017-11-24 14:50:36 +0100

    :ballot_box_with_check: GREEN score pin should not have value less than 0

--- src/main/java/ScorePin.java
+++ src/main/java/ScorePin.java
@@ -2,7 +2,7 @@ public class ScorePin extends Pin {
 
     @Override
     void setValue(int value) {
-        if (value <= 1) {
+        if (value >= 0 && value <= 1) {
             super.value = value;
         } else
             throw new IllegalArgumentException();
