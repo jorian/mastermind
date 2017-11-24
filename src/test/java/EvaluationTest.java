768820b8847a3cbabc967278afb6dfbe55188bed
Jorian <jorian@outlook.com>
2017-11-24 19:59:58 +0100

    :o: RED first evaluation case

---
+++ src/test/java/EvaluationTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EvaluationTest {
+    /*
+    This test class tests several evaluations of guessed codes with secret code. Each test has an explanation
+    in comments to clarify what situation is tested. This in order to prevent very long method names.
+     */
+
+
+    @Test
+    public void case_1() {
+        fail();
+    }
+}
